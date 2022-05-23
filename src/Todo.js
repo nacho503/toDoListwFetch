bc15237430be8be5feecdd205fe8ec73e64d5a56
nacho503 <ignacio.rosenberg.503@gmail.com>
2022-05-23 19:01:15 -0400

    segundo commit

--- src/Todo.js
+++ src/Todo.js
@@ -5,26 +5,8 @@ import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDeleteLeft } from "@fortawesome/free-solid-svg-icons";
 
-//Intento de GET, funciono... ahora intentar hacer un push al arreglo
-// let GetArreglo = () => {
-//   fetch("https://assets.breatheco.de/apis/fake/todos/user/ignacior", {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   })
-//     .then((resp) => {
-//       console.log(resp.ok); // will be true if the response is successfull
-//       console.log(resp.status); // the status code = 200 or code = 400 etc.
-//       console.log(resp.json()); // will try return the exact result as string
-//     })
-//     .catch((error) => {
-//       //error handling
-//       console.log(error);
-//     });
-// };
-
 let Todo = () => {
+  const url = "https://assets.breatheco.de/apis/fake/todos/user/ignacior";
   const [tasks, setTasks] = useState([]);
   const [newTask, setTask] = useState({ label: "", done: false }); //arreglar esta wea primero
 
@@ -35,11 +17,11 @@ let Todo = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setTasks([...tasks, newTask]);
-    console.log("submit ejecutado", newTask, [...tasks]);
+    console.log("submit ejecutado", newTask, tasks);
     setTask({ label: "", done: false });
     // GetArreglo(); //El arreglo anterior solo mostrado en la consola
     //// intento de PUT
-    fetch("https://assets.breatheco.de/apis/fake/todos/user/ignacior", {
+    fetch(url, {
       method: "PUT",
       body: JSON.stringify(tasks),
       headers: {
@@ -50,10 +32,9 @@ let Todo = () => {
         console.log(resp.ok); // will be true if the response is successfull
         console.log(resp.status); // the status code = 200 or code = 400 etc.
         console.log(resp.text()); // will try return the exact result as string
-        // return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
       })
       .then((data) => {
-        setTasks([...tasks]);
         //here is were your code should start after the fetch finishes
         console.log(data); //this will print on the console the exact object received from the server
       })
@@ -67,6 +48,28 @@ let Todo = () => {
     let newArray = tasks;
     newArray.splice(index, 1);
     setTask([...newArray]);
+    /////
+    fetch(url, {
+      method: "PUT",
+      body: JSON.stringify(tasks),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        console.log(resp.ok); // will be true if the response is successfull
+        console.log(resp.status); // the status code = 200 or code = 400 etc.
+        console.log(resp.text()); // will try return the exact result as string
+        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+      })
+      .then((data) => {
+        //here is were your code should start after the fetch finishes
+        console.log(data); //this will print on the console the exact object received from the server
+      })
+      .catch((error) => {
+        //error handling
+        console.log(error);
+      });
   };
 
   return (
