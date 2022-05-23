72f66c61e900c615e8c5c98cbd75595b64b42ceb
nacho503 <ignacio.rosenberg.503@gmail.com>
2022-05-23 18:04:48 -0400

    primer commit

--- src/Todo.js
+++ src/Todo.js
@@ -1,24 +1,66 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./Todo.css";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDeleteLeft } from "@fortawesome/free-solid-svg-icons";
 
+//Intento de GET, funciono... ahora intentar hacer un push al arreglo
+// let GetArreglo = () => {
+//   fetch("https://assets.breatheco.de/apis/fake/todos/user/ignacior", {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   })
+//     .then((resp) => {
+//       console.log(resp.ok); // will be true if the response is successfull
+//       console.log(resp.status); // the status code = 200 or code = 400 etc.
+//       console.log(resp.json()); // will try return the exact result as string
+//     })
+//     .catch((error) => {
+//       //error handling
+//       console.log(error);
+//     });
+// };
+
 let Todo = () => {
   const [tasks, setTasks] = useState([]);
-  const [newTask, setTask] = useState("");
+  const [newTask, setTask] = useState({ label: "", done: false }); //arreglar esta wea primero
 
   const handleChange = (e) => {
-    setTask(e.target.value);
+    setTask({ label: e.target.value, done: false });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setTasks([...tasks, newTask]);
-    console.log("submit ejecutado", newTask, [tasks]);
-    setTask("");
-    // e.target.reset();  <---- no funciono para resetear el placeholder
+    console.log("submit ejecutado", newTask, [...tasks]);
+    setTask({ label: "", done: false });
+    // GetArreglo(); //El arreglo anterior solo mostrado en la consola
+    //// intento de PUT
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/ignacior", {
+      method: "PUT",
+      body: JSON.stringify(tasks),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        console.log(resp.ok); // will be true if the response is successfull
+        console.log(resp.status); // the status code = 200 or code = 400 etc.
+        console.log(resp.text()); // will try return the exact result as string
+        // return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+      })
+      .then((data) => {
+        setTasks([...tasks]);
+        //here is were your code should start after the fetch finishes
+        console.log(data); //this will print on the console the exact object received from the server
+      })
+      .catch((error) => {
+        //error handling
+        console.log(error);
+      });
   };
 
   const deleteTask = (index) => {
@@ -32,7 +74,7 @@ let Todo = () => {
       <ul className="list-group">
         <input
           onChange={handleChange}
-          value={newTask}
+          value={newTask.label}
           type="text"
           className="form-control"
           aria-label="Sizing example input"
@@ -43,11 +85,11 @@ let Todo = () => {
         {tasks.map((task, index) => (
           <li className="list-group-item" key={index}>
             <span>
-              {task}
+              {task.label}
               <FontAwesomeIcon
                 icon={faDeleteLeft}
                 className="deleteIcon"
-                onClick={() => deleteTask(index)} //CUAL ES LA DIFERENCIA DE HACERLO {() => deleteTask(index)} A   deleteTask(index)
+                onClick={() => deleteTask(index)}
               />
             </span>
           </li>
